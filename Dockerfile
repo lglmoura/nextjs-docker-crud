001795780295d698d9d936769e59cb920e74ebcf
Luiz Gustavo L Moura <lglmoura@gmail.com>
2023-06-27 21:51:33 -0300

    feat:docker-compose

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,6 @@ WORKDIR /app
 
 COPY package*.json ./
 COPY prisma ./prisma/
-RUN npm install --force
 COPY . .
  
 EXPOSE 3000
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 const Home = () => {
-  return <div>Home Page - aula 27/06 </div>;
+  return <div>Home Page - aula 28/06 </div>;
 };
 
 export default Home;
